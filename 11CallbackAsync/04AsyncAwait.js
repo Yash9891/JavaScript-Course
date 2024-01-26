733de5e128079c01aa69d044a525296ed4174fa0
Yash <122812129+Yash9891@users.noreply.github.com>
2024-01-26 19:39:39 +0530

    Update 04AsyncAwait.js

--- 11CallbackAsync/04AsyncAwait.js
+++ 11CallbackAsync/04AsyncAwait.js
@@ -3,26 +3,26 @@
 //async function always returns a promise.
 //await pauses the execution of its surrounding async function until the promise is settled.
 
-// async function heloo(){
-//     console.log("Heloo");
-// }
-// heloo()
+async function heloo(){
+    console.log("Heloo");
+}
+heloo()
 
-//asynscronous function because its takes 2 sec
-// function api(){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             console.log("Whether data");
-//             resolve(200);
-//         },2000)
-//     })
-// }
-// async function getWeatherdata(){
-//     await api()//1st call baki niche ke kaam ruk jayenge phele ye kaam hoga
-//     await api()//2nd call
+asynscronous function because its takes 2 sec
+function api(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("Whether data");
+            resolve(200);
+        },2000)
+    })
+}
+async function getWeatherdata(){
+    await api()//1st call baki niche ke kaam ruk jayenge phele ye kaam hoga
+    await api()//2nd call
 
-// }
-// getWeatherdata()
+}
+getWeatherdata()
 /** Async is used to perform the task step by step in synchronous way.
  * Async await is used for the work such as to execute the work 1 and then execute the work 2 
  * Normally in asynchronous coding work 1 is execute and  work 2 takes 3 sec to execute but work 3 execute before the work 2 to solve this we use async await to stop the work 3 execution before work 2
