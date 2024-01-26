2795b0da6790075850e3d3292abf1c96a346a8d0
Yash <122812129+Yash9891@users.noreply.github.com>
2024-01-26 19:32:13 +0530

    Update 01functions.js

--- 05Functions/01functions.js
+++ 05Functions/01functions.js
@@ -2,28 +2,28 @@
 
 //Function defination
 
-// function functionname(){
-//     //do some work
-// }
+function functionname(){
+    //do some work
+}
 
-// functionname()//call
+functionname()//call
 
 
 //*********************************** */
 
-// function college(msg){//parameters
-//     console.log(msg);
-// }
-// college("Welcome to kong land stay safe from godzilla")//arguments
+function college(msg){//parameters
+    console.log(msg);
+}
+college("Welcome to kong land stay safe from godzilla")//arguments
 
 
-// function sum( a, b){
-//     return a+b;
-// }
-// let x=3;
-// let y=9.56;
+function sum( a, b){
+    return a+b;
+}
+let x=3;
+let y=9.56;
 
-// console.log(sum(x,y));
+console.log(sum(x,y));
 
 
 
@@ -31,30 +31,30 @@
 
 //Arrow functions:compact way to writing functions
 
-// const nami=()=>{
-//     console.log("hlo")
-// }
-// nami();
+const nami=()=>{
+    console.log("hlo")
+}
+nami();
 
-// const mul=(a,b)=>{
-//     return a*b;
-// }
-// console.log(mul(2,45));
+const mul=(a,b)=>{
+    return a*b;
+}
+console.log(mul(2,45));
 
 //function to return vovels
 
-// const countVowels=(str)=> {
-//     let count = 0;
-//     for (let i = 0; i < str.length; i++) {
-//         if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
-//             count++;
-//         }
-//     }
-//     console.log(count);
-// }
+const countVowels=(str)=> {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
+            count++;
+        }
+    }
+    console.log(count);
+}
 
-// let str = "ApnaGozdilla";
-// countVowels(str);
+let str = "ApnaGozdilla";
+countVowels(str);
 
 
 
