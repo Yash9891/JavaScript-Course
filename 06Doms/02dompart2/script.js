a1a054f4407eaf53c22a0f1dbb2e8fd6553c027a
Yash <122812129+Yash9891@users.noreply.github.com>
2024-01-26 19:33:55 +0530

    Update script.js

--- 06Doms/02dompart2/script.js
+++ 06Doms/02dompart2/script.js
@@ -5,14 +5,14 @@
  */
 
  //get the attribute
-//  let div= document.querySelector('div')
-//  console.log(div);
-// console.log(div.getAttribute('id'));
-// console.log(div.getAttribute('name'));
+ let div= document.querySelector('div')
+ console.log(div);
+console.log(div.getAttribute('id'));
+console.log(div.getAttribute('name'));
 
-// let para=document.querySelector('p')
-// console.log(para.getAttribute('class'))
-// console.log(para.setAttribute('class','newclass'))
+let para=document.querySelector('p')
+console.log(para.getAttribute('class'))
+console.log(para.setAttribute('class','newclass'))
 
 
 ///************style ***************/
