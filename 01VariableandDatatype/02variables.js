31c3b300d32115132404400a5589ba89da52aa3e
Yash <122812129+Yash9891@users.noreply.github.com>
2024-01-26 19:26:41 +0530

    Update 02variables.js

--- 01VariableandDatatype/02variables.js
+++ 01VariableandDatatype/02variables.js
@@ -1,26 +1,26 @@
 //Variables are container for data
 
-// age =24
-// name ="Tony"
-// x=null; //pata hai iske andar kya hai but iske andar aek khali value hai
-// y=undefined; //pata hi nhi iske andar kya hai
-// isfollow=false;
-// console.log(age,name);
-// name=78
-// console.log(x,y,isfollow);
-// console.log(name);
+age =24
+name ="Tony"
+x=null; //pata hai iske andar kya hai but iske andar aek khali value hai
+y=undefined; //pata hi nhi iske andar kya hai
+isfollow=false;
+console.log(age,name);
+name=78
+console.log(x,y,isfollow);
+console.log(name);
 
-// $name="Gojira"
-// _name="Gojira"
-// console.log(_name);
+$name="Gojira"
+_name="Gojira"
+console.log(_name);
 
 //var: Variable can be re-declareed and updated.A global scope variable
 //let: Variable cannot be re-declareed but updated.A block scope variable
 //const: Variable cannot be re-declareed or updated.A block scope variable
 
-// let name="Superman"
-// name="Bodi"
-// console.log(name);
+let name="Superman"
+name="Bodi"
+console.log(name);
 
 const name="Superman"
 // name="Bodi" //cannot updated
